d89dc1dd92d07cca81d33905892028325f853cd3
andrea_cajamarca <andreakcajamarca@gmail.com>
2017-11-06 22:47:25 +0000

    append and attr with firebase

--- public/script.js
+++ public/script.js
@@ -5,16 +5,4 @@
 // *************************** YOUR CODE BELOW *******************************
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
-$(document).ready(function() {
 
-    var projects = snapshot.val();
-    for (var i in projects) {
-        var div = $('<div>');
-        var title = $('<h1>');
-        title.text(projects[i].title);
-        div.append(title);
-        $("#project-sec").append(div);
-    }
-
-
-});
